fc4784430d128ab69c6a2c703a3a827633707db1
Sheittanis <shei_ellas@hotmail.com>
2016-11-01 23:59:12 +0000

    Added sound volumes
    Adjusted scripts for enemy class on next push

---
+++ v1.2/Assets/s_Music.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Music : MonoBehaviour
+{
+
+    static bool AudioPlaying = false;
+    public bool mute = false; //used for muting
+
+
+    public AudioSource source;
+    void Awake()
+    {
+        if (!AudioPlaying)
+        {
+            GetComponent<AudioSource>().Play();
+            DontDestroyOnLoad(gameObject);
+            AudioPlaying = true;
+        }
+    }
+
+    public void VolumeUP()
+    {
+        source.volume += 0.1f;
+        for (int i = 0; i < 5; i++)
+        {
+            source.volume += 0.1f;
+        }
+
+    }
+
+    public void VolumeDOWN()
+    {
+        source.volume -= 0.1f;
+        for (int i = 0; i < 5; i++)
+        {
+            source.volume -= 0.1f; //vol control
+        }
+
+    }
+
+    public void MuteMusic()
+    {
+        if (source.mute)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                source.mute = false; //mute
+                source.mute = false;
+            }
+        }
+
+        else
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                source.mute = true;
+                source.mute = true;
+            }
+        }
+    }
+}
---
+++ v1.2/Assets/s_singleton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_singleton : MonoBehaviour
+{
+
+    private static s_singleton instance = null;
+
+    public static s_singleton Instance
+    {
+        get { return instance; }
+    }
+
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
