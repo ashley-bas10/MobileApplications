0712fb90a4cfd5009a7a3a0cb73fa1d54e0da303
DROFNEBMAS <samuelbenford@hotmail.co.uk>
2016-11-01 13:31:14 +0000

    Menus implemented
    Main Menu with buttons that go to Tutorial/Level Select Screen, Options
    pop up menu, shop screen.
    
    Pause Menu that stops the game brings up the pause pop up menu, with
    buttons that return to game, return to menu and bring up the options pop
    up menu.

---
+++ v1.2/Assets/ButtonManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManagerScript : MonoBehaviour {
+
+    int PlayedGameCheck = 0;
+
+    public GameObject MainCanvas;
+    public GameObject OptionsCanvas;
+
+    public void StartGame ()
+        {
+           PlayedGameCheck = PlayerPrefs.GetInt("PlayedGameCheck");
+
+           if (PlayedGameCheck == 0)
+             {
+                 Application.LoadLevel("CharSelect");
+
+                PlayerPrefs.SetInt("PlayedGameCheck", 1);
+             }
+           else
+             {
+                Application.LoadLevel("LevelSelect");
+
+                //PlayerPrefs.SetInt("PlayedGameCheck", 0);
+
+             }
+        }
+    public void Options ()
+    {
+        MainCanvas.SetActive(false);
+        OptionsCanvas.SetActive(true);
+    }
+
+    public void Shop ()
+    {
+        Application.LoadLevel("Shop");
+    }
+
+    public void backtomenu ()
+    {
+        MainCanvas.SetActive(true);
+        OptionsCanvas.SetActive(false);
+    }
+}
---
+++ v1.2/Assets/PauseMenuScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuScript : MonoBehaviour {
+    public GameObject PauseCanvas;
+
+    public GameObject OptionsCanvas;
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            PauseCanvas.SetActive(true);
+        }
+	}
+
+    public void ReturnToGame()
+    {
+        Time.timeScale = 1;
+        PauseCanvas.SetActive(false);
+    }
+
+    public void Options ()
+    {
+        PauseCanvas.SetActive(false);
+        OptionsCanvas.SetActive(true);
+
+    }
+    
+    public void BackToMainMenu ()
+    {
+        Application.LoadLevel("Main Menu");
+    }
+
+  
+
+    public void BackToPause()
+    {
+        PauseCanvas.SetActive(true);
+        OptionsCanvas.SetActive(false);
+    }
+}
